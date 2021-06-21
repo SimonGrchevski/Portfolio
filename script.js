f079c59d0e2f7d2badbfb1a24f73c55145ba6809
SimonGrchevski <simongrcevski@gmail.com>
2021-06-21 15:07:40 +0200

    Fix indentation errors

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-let list = document.getElementById('menu-list');
-let hamX = document.getElementById('ham_x');
-let x = window.matchMedia('(min-width: 1024px)');
+const list = document.getElementById('menu-list');
+const hamX = document.getElementById('ham_x');
+const screenSize = window.matchMedia('(min-width: 1024px)');
 
-function phone(x) {
-    if (x.matches) { // If media query matches
-        list.style.display = 'flex';
-        hamX.className = 'fa fa-bars menu-icon';
-    } else {
-        list.style.display = 'none';
-    }
+function phone(screenSize) {
+  if (screenSize.matches) { // If media query matches
+    list.style.display = 'flex';
+    hamX.className = 'fa fa-bars menu-icon';
+  } else {
+    list.style.display = 'none';
+  }
 }
-phone(x);
-x.addListener(phone);
-hamX.addEventListener('click', function show() {
-    if (hamX.className === 'fa fa-bars menu-icon') {
-        hamX.className = 'fa fa-times menu-icon';
-        list.style.display = 'block';
-    } else {
-        hamX.className = 'fa fa-bars menu-icon';
-        list.style.display = 'none';
-    }
+phone(screenSize);
+screenSize.addListener(phone);
+hamX.addEventListener('click', function() {
+  if (hamX.className === 'fa fa-bars menu-icon') {
+    hamX.className = 'fa fa-times menu-icon';
+    list.style.display = 'block';
+  } else {
+    hamX.className = 'fa fa-bars menu-icon';
+    list.style.display = 'none';
+  }
 });
