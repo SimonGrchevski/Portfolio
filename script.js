32cc83c946677ca44a1660bf980a953c556a314f
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-06-21 11:39:05 -0400

    Finally Fix All Linter Errors

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ const menuIcon = document.querySelector('.menu-icon');
 const abs = document.querySelectorAll('.abs');
 const welcome = document.querySelector('.welcome');
 
+function removeBackground(abs) {
+  Array.from(abs).forEach((element) => {
+    element.style.display = 'none';
+  });
+}
+
+function addBackground(abs) {
+  Array.from(abs).forEach((element) => {
+    element.style.position = 'absolute';
+    element.style.display = 'block';
+  });
+}
+
 function phone(screenSize) {
   if (screenSize.matches) {
     list.style.display = 'flex';
