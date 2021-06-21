0f1fbbdd8931a05c2a5a1fc3d9d75318c61aeaef
SimonGrchevski <simongrcevski@gmail.com>
2021-06-21 14:59:26 +0200

    Fix EsLinter errors

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-var list = document.getElementById("menu-list");
-var menu = document.getElementById("nav");
-var ham_x = document.getElementById("ham_x")
-var x = window.matchMedia("(min-width: 1024px)")
+let list = document.getElementById('menu-list');
+let hamX = document.getElementById('ham_x');
+let x = window.matchMedia('(min-width: 1024px)');
 
 function phone(x) {
     if (x.matches) { // If media query matches
-        list.style.display = "flex"
-        ham_x.className = "fa fa-bars menu-icon";
+        list.style.display = 'flex';
+        hamX.className = 'fa fa-bars menu-icon';
     } else {
-        list.style.display = "none";
+        list.style.display = 'none';
     }
 }
-phone(x)
-x.addListener(phone)
-ham_x.addEventListener("click", function show() {
-    if (ham_x.className == "fa fa-bars menu-icon") {
-        ham_x.className = "fa fa-times menu-icon";
-        list.style.display = "block"
+phone(x);
+x.addListener(phone);
+hamX.addEventListener('click', function show() {
+    if (hamX.className === 'fa fa-bars menu-icon') {
+        hamX.className = 'fa fa-times menu-icon';
+        list.style.display = 'block';
     } else {
-        ham_x.className = "fa fa-bars menu-icon";
-        list.style.display = "none"
+        hamX.className = 'fa fa-bars menu-icon';
+        list.style.display = 'none';
     }
 });
