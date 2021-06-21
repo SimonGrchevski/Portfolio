5c6ca3756f72e41b00cdad9e7a3c75450a2079f2
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-06-21 08:35:58 -0400

    Add menu display

---
+++ script.js
@@ -0,0 +1,16 @@
+var list = document.getElementById("menu-list");
+var menu = document.getElementById("nav");
+var ham_x = document.getElementById("ham_x")
+var x = window.matchMedia("(min-width: 1024px)")
+
+function phone(x) {
+    if (x.matches) { // If media query matches
+        list.style.display="flex"
+        ham_x.className = "fa fa-bars menu-icon";
+    }else{
+        list.style.display="none";
+    }
+}
+  phone(x) 
+  x.addListener(phone) 
+);
