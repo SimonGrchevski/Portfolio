28f559442d6abf07a9fd766309aa4d8f6a9071e7
SimonGrchevski <simongrcevski@gmail.com>
2021-06-21 14:37:19 +0200

    Add event listener

--- script.js
+++ script.js
@@ -5,12 +5,20 @@ var x = window.matchMedia("(min-width: 1024px)")
 
 function phone(x) {
     if (x.matches) { // If media query matches
-        list.style.display="flex"
+        list.style.display = "flex"
         ham_x.className = "fa fa-bars menu-icon";
-    }else{
-        list.style.display="none";
+    } else {
+        list.style.display = "none";
     }
 }
-  phone(x) 
-  x.addListener(phone) 
-);
+phone(x)
+x.addListener(phone)
+ham_x.addEventListener("click", function show() {
+    if (ham_x.className == "fa fa-bars menu-icon") {
+        ham_x.className = "fa fa-times menu-icon";
+        list.style.display = "block"
+    } else {
+        ham_x.className = "fa fa-bars menu-icon";
+        list.style.display = "none"
+    }
+});
