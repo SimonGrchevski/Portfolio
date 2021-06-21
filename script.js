f7c6bb6e9d3a069a95224fdbddf9d99033582728
SimonGrchevski <simongrcevski@gmail.com>
2021-06-21 16:27:54 +0200

    Remove the background when the menu is oppened

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const list = document.getElementById('menu-list');
 const hamX = document.getElementById('ham_x');
 const screenSize = window.matchMedia('(min-width: 1024px)');
-const menu_icon = document.querySelector(".menu-icon");
-const abs = document.querySelector(".abs");
+const menu_icon = document.querySelector('menu-icon');
+const abs = document.querySelectorAll('.abs');
+const welcome = document.querySelector('.welcome');
 
+console.log(welcome);
+
+
+function removeBackground(abs) {
+  Array.from(abs).forEach(element => {
+    element.style.display = "none";
+  });
+}
+
+function addBackground(abs) {
+  Array.from(abs).forEach(element => {
+    element.style.position = "absolute";
+    element.style.display = 'block';
+  });
+}
 function phone(screenSize) {
   if (screenSize.matches) { // If media query matches
     list.style.display = 'flex';
     hamX.className = 'fa fa-bars menu-icon';
-    hamX.style.color= "black";
+    hamX.style.color = "black";
     list.classList.remove('open_menu');
+    addBackground(abs);
   } else {
     list.style.display = 'none';
     list.classList.add('open_menu');
@@ -22,14 +39,15 @@ hamX.addEventListener('click', show);
 function show() {
   if (hamX.className === 'fa fa-bars menu-icon') {
     hamX.className = 'fa fa-times menu-icon';
-    hamX.style.color= "grey";
+    hamX.style.color = "grey";
     list.style.display = 'block';
     menu_icon.style.marginLeft = '45vw';
-    abs.style.display="none"
+    // remove background
+    removeBackground(abs);
   } else {
     hamX.className = 'fa fa-bars menu-icon';
-    hamX.style.color= "black";
+    hamX.style.color = "black";
     list.style.display = 'none';
-    abs.style.display="absolute";
+    addBackground(abs);
   }
 }
