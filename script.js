3c11ac53b6f124f52bfdc4dcc8564ded88e7c3e1
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-06-21 11:37:05 -0400

    Fix Linter Error VII

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ const menuIcon = document.querySelector('.menu-icon');
 const abs = document.querySelectorAll('.abs');
 const welcome = document.querySelector('.welcome');
 
-function removeBackground(abs) {
-  Array.from(abs).forEach((element) => {
-    element.style.display = 'none';
-  });
-}
-
-function addBackground(abs) {
-  Array.from(abs).forEach((element) => {
-    element.style.position = 'absolute';
-    element.style.display = 'block';
-  });
+function phone(screenSize) {
+  if (screenSize.matches) {
+    list.style.display = 'flex';
+    hamX.className = 'fa fa-bars menu-icon';
+    hamX.style.color = 'black';
+    list.classList.remove('open_menu');
+    addBackground(abs);
+    welcome.style.display = 'none';
+  } else {
+    welcome.style.display = 'block';
+    list.style.display = 'none';
+    list.classList.add('open_menu');
+  }
 }
 
 function show() {
@@ -35,5 +37,7 @@ function show() {
     addBackground(abs);
   }
 }
+
+phone(screenSize);
 screenSize.addListener(phone);
 hamX.addEventListener('click', show);
