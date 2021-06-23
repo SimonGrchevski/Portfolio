19856975bb88fab60f757caeb8dfb74a4b290a34
SimonGrchevski <simongrcevski@gmail.com>
2021-06-23 16:36:38 +0200

    Add the dynamic population for cards

--- popup.js
+++ popup.js
@@ -3,9 +3,9 @@ let tags = [{
   classes: ["card", "flex", "column", "width100"],
   attr: [""],
   child: [
-    new Tag("div", ["card-image", "width100"], [], [], ""),
-    new Tag("section", ["img-info", "width100"], [], [], ""),
-    new Tag("h2", ["img-info", "width100"], [], [], "Multi-Post Stories<br>Gain+Glory"),
+    new Tag("div", ["card-image", "width100", "height50"], [], [], ""),
+    new Tag("section", ["img-info", "flex", "column", "height50"], [], [], ""),
+    new Tag("h2", [], [], [], "Multi-Post Stories Gain+Glory"),
     new Tag("ul", ["flex", "ul-skills"], [], [], ""),
     new Tag("li", [], [], [], ""),
     new Tag("a", [], ["#"], [], "Ruby"),
@@ -25,11 +25,95 @@ let tags = [{
   attr: [""],
   child: [
     new Tag("div", ["card-image", "width100", "height50"], [], [], ""),
-    new Tag("section", ["img-info", "flex", "column"], [], [], ""),
-    new Tag("h2", ["img-info", "width100"], [], [], "Multi-Post Stories<br>Gain+Glory"),
+    new Tag("section", ["img-info", "flex", "column", "height50"], [], [], ""),
+    new Tag("h2", [], [], [], "Multi-Post Stories Gain+Glory"),
     new Tag("ul", ["flex", "ul-skills"], [], [], ""),
     new Tag("li", [], [], [], ""),
-    new Tag("a", [], [], [], "Ruby"),
+    new Tag("a", [], ["#"], [], "Ruby"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "CSS"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "JavaScript"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "html"),
+    new Tag("a", ["btn-more"], [], [], "See Project")
+  ],
+  value: [""]
+},
+{
+  tagName: "article",
+  classes: ["card", "flex", "column", "width100"],
+  attr: [""],
+  child: [
+    new Tag("div", ["card-image", "width100", "height50"], [], [], ""),
+    new Tag("section", ["img-info", "flex", "column", "height50"], [], [], ""),
+    new Tag("h2", [], [], [], "Multi-Post Stories Gain+Glory"),
+    new Tag("ul", ["flex", "ul-skills"], [], [], ""),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "Ruby"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "CSS"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "JavaScript"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "html"),
+    new Tag("a", ["btn-more"], [], [], "See Project")
+  ],
+  value: [""]
+},
+{
+  tagName: "article",
+  classes: ["card", "flex", "column", "width100"],
+  attr: [""],
+  child: [
+    new Tag("div", ["card-image", "width100", "height50"], [], [], ""),
+    new Tag("section", ["img-info", "flex", "column", "height50"], [], [], ""),
+    new Tag("h2", [], [], [], "Multi-Post Stories Gain+Glory"),
+    new Tag("ul", ["flex", "ul-skills"], [], [], ""),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "Ruby"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "CSS"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "JavaScript"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "html"),
+    new Tag("a", ["btn-more"], [], [], "See Project")
+  ],
+  value: [""]
+},
+{
+  tagName: "article",
+  classes: ["card", "flex", "column", "width100"],
+  attr: [""],
+  child: [
+    new Tag("div", ["card-image", "width100", "height50"], [], [], ""),
+    new Tag("section", ["img-info", "flex", "column", "height50"], [], [], ""),
+    new Tag("h2", [], [], [], "Multi-Post Stories Gain+Glory"),
+    new Tag("ul", ["flex", "ul-skills"], [], [], ""),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "Ruby"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "CSS"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "JavaScript"),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "html"),
+    new Tag("a", ["btn-more"], [], [], "See Project")
+  ],
+  value: [""]
+},
+{
+  tagName: "article",
+  classes: ["card", "flex", "column", "width100"],
+  attr: [""],
+  child: [
+    new Tag("div", ["card-image", "width100", "height50"], [], [], ""),
+    new Tag("section", ["img-info", "flex", "column", "height50"], [], [], ""),
+    new Tag("h2", [], [], [], "Multi-Post Stories Gain+Glory"),
+    new Tag("ul", ["flex", "ul-skills"], [], [], ""),
+    new Tag("li", [], [], [], ""),
+    new Tag("a", [], ["#"], [], "Ruby"),
     new Tag("li", [], [], [], ""),
     new Tag("a", [], ["#"], [], "CSS"),
     new Tag("li", [], [], [], ""),
@@ -49,31 +133,46 @@ function Tag(tagName, classes, attr, child, value) {
   this.value = value;
 }
 
-tags.forEach(tag => {
+const cardContainer = document.querySelector(".card-container");
 
-  let article = document.createElement(tag.tagName);
-  article.classList.add(...tag.classes);
+function generateCards() {
 
-  let childElements = [];
-  tag.child.forEach(childItem => {
-    let element = document.createElement(childItem.tagName);
-    childItem.classes.forEach(childClass => {
-      element.classList.add(childClass);
+  let articles = [];
+  tags.forEach(tag => {
+
+    let article = document.createElement(tag.tagName);
+    article.classList.add(...tag.classes);
+
+    let childElements = [];
+    tag.child.forEach(childItem => {
+      let element = document.createElement(childItem.tagName);
+      childItem.classes.forEach(childClass => {
+        element.classList.add(childClass);
+      });
+      element.textContent = childItem.value;
+      element.href = childItem.attr;
+      childElements.push(element);
     });
-    element.textContent = childItem.value;
-    element.href = childItem.attr;
-    childElements.push(element);
-  });
 
-  article.append(childElements[0], childElements[1]);
-  childElements[1].append(childElements[2], childElements[3], childElements[12]);
+    article.append(childElements[0], childElements[1]);
+    childElements[1].append(childElements[2], childElements[3], childElements[12]);
 
-  for (let li = 4; li <= 10; li += 2) {
-    childElements[li].append(childElements[li + 1]);
-    childElements[3].append(childElements[li]);
+    for (let li = 4; li <= 10; li += 2) {
+      childElements[li].append(childElements[li + 1]);
+      childElements[3].append(childElements[li]);
+    }
+    articles.push(article);
+  })
+  return articles;
+}
+
+function populateCards(cards) {
+  for (let i = 0; i < cards.length; ++i) {
+    let li = document.createElement('li');
+    li.append(cards[i]);
+    cardContainer.append(li);
   }
+}
 
-  console.log(article);
-
-  document.getElementById("test").append(article);
-})
+let cards = generateCards();
+populateCards(cards);
