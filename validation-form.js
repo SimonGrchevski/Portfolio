48a09087eb8f15a3708b0d3259b7d168fc2e74e6
SimonGrchevski <simongrcevski@gmail.com>
2021-06-25 16:35:20 +0200

    Fix linter errors

--- validation-form.js
+++ validation-form.js
@@ -1,30 +1,27 @@
-let form = document.getElementById('get-in-touch-form');
-const email = form.elements['email'];
-const err = 'Email content must be lower case';
-const msg_elem = document.getElementById('msg');
+const form = document.getElementById('get-in-touch-form');
+const { email } = form.elements;
+const msgElem = document.getElementById('msg');
 const eSuccess = 0;
 const eFailure = 1;
 const eventInfo = [
-    { cl: 'success', msg: 'You successfully submited the form' },
-    { cl: 'fail', msg: 'The email input must be in lower case' }
+  { cl: 'success', msg: 'You successfully submited the form' },
+  { cl: 'fail', msg: 'The email input must be in lower case' },
 ];
 
 function displayResponse(eInfo) {
-    msg_elem.className = eInfo['cl'];
-    msg_elem.innerText = eInfo['msg'];
+  msgElem.className = eInfo.cl;
+  msgElem.innerText = eInfo.msg;
 }
 
 function validEmail() {
-    return (/^[^A-Z]*$/).test(email.value);
+  return (/^[^A-Z]*$/).test(email.value);
 }
 
 form.addEventListener('submit', (e) => {
-
-    if (validEmail()) {
-        displayResponse(eventInfo[eSuccess])
-    }
-    else {
-        displayResponse(eventInfo[eFailure])
-        e.preventDefault();
-    }
-})
+  if (validEmail()) {
+    displayResponse(eventInfo[eSuccess]);
+  } else {
+    displayResponse(eventInfo[eFailure]);
+    e.preventDefault();
+  }
+});
