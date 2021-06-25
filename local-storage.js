da43ed9685226ee89d6d9a43330ce3dea3664aea
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-06-25 15:07:25 -0400

    Fix Saving Inside LocalStorage

--- local-storage.js
+++ local-storage.js
@@ -1,14 +1,28 @@
 const form = document.getElementById('get-in-touch-form');
 const nameField = form.elements['name'];
 const emailField = form.elements['email'];
-let userData = {};
 
-if (localStorage.getItem('user')) {
+function UserData(name, email){
+  this.name=name;
+  this.email=email;
+}
+
+let userData = JSON.parse(localStorage.getItem('user'))
+
   console.log(JSON.parse(localStorage.getItem('user')));
-  userData = JSON.parse(localStorage.getItem['user']);
   nameField.value = userData.name;
   emailField.value = userData.email;
-}
+
+
+form.addEventListener('submit', function (e) {
+
+  if (!localStorage.getItem('user')) {
+    userData = new UserData(nameField.value, emailField.value)
+    localStorage.setItem('user', JSON.stringify(userData));
+  }
+  e.preventDefault();
+});
+
 
 
 
