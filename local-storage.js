ee538f6d4048fc233c6b81a3bb66956bf4fa49ca
SimonGrchevski <simongrcevski@gmail.com>
2021-06-26 00:14:56 +0200

    Fix the localstorage error and merge the two files

--- local-storage.js
+++ local-storage.js
@@ -2,27 +2,36 @@ const form = document.getElementById('get-in-touch-form');
 const nameField = form.elements['name'];
 const emailField = form.elements['email'];
 
-function UserData(name, email){
+function UserData(name='', email=''){
   this.name=name;
   this.email=email;
 }
 
-let userData = JSON.parse(localStorage.getItem('user'))
+let userData = JSON.parse(localStorage.getItem('user'));
+nameField.value = userData.name;
+emailField.value = userData.email;
 
-  console.log(JSON.parse(localStorage.getItem('user')));
-  nameField.value = userData.name;
-  emailField.value = userData.email;
+nameField.addEventListener('onchange', function () {
+  userData = new UserData(nameField,userData.email);
+  updataLocalStorage();
+})
 
+emailField.addEventListener('onchange', function(){
+  userData = new UserData(userData.name,userData.email);
+  updataLocalStorage();
+})
 
 form.addEventListener('submit', function (e) {
-
+  e.preventDefault();
   if (!localStorage.getItem('user')) {
     userData = new UserData(nameField.value, emailField.value)
-    localStorage.setItem('user', JSON.stringify(userData));
+    updataLocalStorage();
   }
-  e.preventDefault();
 });
 
+function updataLocalStorage(){
+  localStorage.setItem('user', JSON.stringify(userData));
+}
 
 
 
--- validation-form.js
+++ validation-form.js
@@ -1,6 +1,9 @@
 const form = document.getElementById('get-in-touch-form');
-const { email } = form.elements;
+const nameField = form.elements['name'];
+const emailField = form.elements['email'];
+const textField = form.elements['message'];
 const msgElem = document.getElementById('msg');
+let userData = {};
 const eSuccess = 0;
 const eFailure = 1;
 const eventInfo = [
@@ -8,20 +11,60 @@ const eventInfo = [
   { cl: 'fail', msg: 'The email input must be in lower case' },
 ];
 
+function UserData(name = '', email = '', area='') {
+  this.name = name;
+  this.email = email;
+  this.area = area;
+}
+
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (e.code === 22 || e.code === 1014 || e.name === 'QuotaExceededError' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED') && (storage && storage.length !== 0);
+  }
+}
+
+if (storageAvailable('localStorage')) {
+  userData = JSON.parse(localStorage.getItem('userData'));
+  if (userData === null) {
+    userData = new UserData(nameField.value, emailField.value, textField.value);
+  }
+  nameField.value = userData.name;
+  emailField.value = userData.email;
+  textField.value = userData.area;
+}
+
 function displayResponse(eInfo) {
   msgElem.className = eInfo.cl;
   msgElem.innerText = eInfo.msg;
 }
 
 function validEmail() {
-  return (/^[^A-Z]*$/).test(email.value);
+  return (/^[^A-Z]*$/).test(emailField.value);
 }
 
 form.addEventListener('submit', (e) => {
+  e.preventDefault();
   if (validEmail()) {
     displayResponse(eventInfo[eSuccess]);
   } else {
     displayResponse(eventInfo[eFailure]);
-    e.preventDefault();
   }
 });
+
+[emailField,nameField,textField].forEach((elem) => {
+  elem.addEventListener('change', function () {
+    userData = new UserData(nameField.value, emailField.value, textField.value);
+    updataLocalStorage();
+  });
+});
+
+function updataLocalStorage() {
+  localStorage.setItem('user', JSON.stringify(userData));
+}
